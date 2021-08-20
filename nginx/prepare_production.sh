5694e55bda9571cbff9aee4f28240273c831cb77
Percival Rapha <percival.rapha@gmail.com>
2021-08-20 23:45:41 +0200

    Node sass add

--- nginx/prepare_production.sh
+++ nginx/prepare_production.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/frappe/frappe-bench/apps/frappe && yarn install --production=true && yarn install node-sass
+cd /home/frappe/frappe-bench/apps/frappe && yarn install --production=true && yarn add node-sass
 rm /home/frappe/frappe-bench/sites/apps.txt
